50d29b273e1212273ea48f5ee7cf4cfcee538d62
Alazar Lemma <alazar03lemma@gmail.com>
2023-08-19 05:32:47 +0000

    used the before to follow dry principle in my code

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,6 +1,19 @@
-RSpec.describe 'Card' do
-    it 'has a type' do
-        card = Card.new('Ace of spades')##The 'Ace of spade' <= the type of the card
-        expect(card.type).to eq('Ace of spades')
+class Student
+    attr_reader :name, :email
+    def initialize(name, email)
+        @name = name
+        @email = email
+    end
+end
+
+RSpec.describe 'Student' do
+    before do
+        @student = Student.new('Abebe','abebe@gmail.com')
+    end
+    it 'has a name' do
+        expect(@student.name).to eq('Abebe')
+    end
+    it 'has an email' do
+        expect(@student.email).to eq('abebe@gmail.com')
     end
 end
