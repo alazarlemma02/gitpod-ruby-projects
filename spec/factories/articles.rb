c7db03f9debc48d41425c0f24ad5c7c40c743656
alazarlemma02 <alazar03lemma@gmail.com>
2023-09-02 14:49:07 +0300

    made the content factory to return sentence rather than a word

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
     title{Faker::Name.name}
-    content{Faker::Lorem.word}
+    content{Faker::Lorem.sentence}
     slug{Faker::Alphanumeric.alpha(number: 3)}
   end
 end
