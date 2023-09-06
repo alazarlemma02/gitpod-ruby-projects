dc3541b7b1d96622ecb56aa25fdfd6a5fd3e08b9
alazarlemma02 <alazar03lemma@gmail.com>
2023-09-06 14:15:16 +0300

    structure the validation and association test for the two models

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  describe "#validation" do
-    it{should belong_to(:category) }
-    it{should validate_presence_of(:title)}
-    it{should validate_presence_of(:author)}
-    it{should validate_length_of(:title).is_at_least(3)}
+  describe "#association + validation test for book model" do
+    #association test'
+    context "association test" do
+      it{should belong_to(:category) }
+    end
+    #validation test
+    context "validation test" do
+      it{should validate_presence_of(:title)}
+      it{should validate_presence_of(:author)}
+      it{should validate_length_of(:title).is_at_least(3)}
+    end
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  describe "#validation" do
+  describe "validation + association test for cateogry model" do
     #association test
-    it{should have_many(:book)}
+    context "association test" do
+      it{should have_many(:book)}
+    end
     #validation test
-    it{should validate_presence_of(:name)}
-    it{should validate_length_of(:name).is_at_least(3)}
+    context "validation test" do
+      it{should validate_presence_of(:name)}
+      it{should validate_length_of(:name).is_at_least(3)}
+    end
+
   end
 
 end
